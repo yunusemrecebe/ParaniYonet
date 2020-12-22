c442fc4a73ea19c3de3b8b42e79ec595192132ef
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2020-12-22 13:17:50 +0300

    kategoriler ekranı için backend başlangıcı

--- backend/functions.php
+++ backend/functions.php
@@ -1,3 +1,26 @@
 <?php
 date_default_timezone_set('Europe/Istanbul');
+
+    function ViewSumSpendingByCategory($x){
+
+        try {
+            $db = new PDO("mysql:host=localhost;dbname=paraniyonet;charset=utf8", "root", "");
+
+        } catch ( PDOException $e ){
+            print $e->getMessage();
+        }
+
+
+        $userId = $_SESSION['userId'];
+        $spendings = $db->query("SELECT Sum(s.Amount) as Total FROM Spendings s JOIN Accounts a ON s.Account = a.Id JOIN Users u ON u.Id = a.Owner WHERE u.Id = $userId AND s.Category = $x")->fetch(PDO::FETCH_ASSOC);
+        if ($spendings['Total']>0){
+            echo $spendings['Total'];
+        }
+        else{
+            echo "0";
+        }
+
+
+}
+
 ?>
--- kategoriler.php
+++ kategoriler.php
@@ -5,17 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style/kategoriler.css" />
     <link rel="icon" type="image/png" href="assets/calculate.png" />
-    <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-  />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-      crossorigin="anonymous"
-    />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
     <title>Paranı Yönet - Kategoriler</title>
   </head>
 
@@ -23,59 +14,57 @@
     <div class="main-content">
       <div class="sidebar">
         <ul class="list-group">
-          <a href="hesapDetay.php"
-            ><li class="list-group-item p-3">Hesap İşlemleri</li></a
-          >
-          <a href="hareketler.php"
-            ><li class="list-group-item p-3">Hareketler</li></a
-          ><a href="kategoriler.php"
-            ><li class="list-group-item active p-3 animate__animated animate__fadeIn">Kategoriler</li></a
-          >
+          <a href="hesapDetay.php"><li class="list-group-item p-3">Hesap İşlemleri</li></a>
+          <a href="hareketler.php"><li class="list-group-item p-3">Hareketler</li></a>
+          <a href="kategoriler.php"><li class="list-group-item active p-3 animate__animated animate__fadeIn">Kategoriler</li></a>
           <a href="genelBakis.php"><li class="list-group-item p-3">Genel Bakış</li></a>
-
-          <a href="kullaniciIslemleri.php"
-            ><li class="list-group-item p-3">Kullanıcı İşlemleri</li></a
-          >
+          <a href="kullaniciIslemleri.php"><li class="list-group-item p-3">Kullanıcı İşlemleri</li></a>
         </ul>
       </div>
+
+<?php
+require_once 'backend/dbconnect.php';
+require_once 'backend/functions.php';
+?>
 
       <div class="main-right">
         <div class="circle-card">
           <img src="assets/fuel.png" />
           <h4>Akaryakıt</h4>
-          <h1>200TL</h1>
+          <h1>
+             <?php ViewSumSpendingByCategory(1); ?></h1>
         </div>
 
         <div class="circle-card">
           <img src="assets/healt.png" />
           <h4>Sağlık</h4>
-          <h1>200TL</h1>
+            <h1><?php ViewSumSpendingByCategory(2); ?></h1>
         </div>
 
         <div class="circle-card">
           <img src="assets/market.png" />
           <h4>Market</h4>
-          <h1>200TL</h1>
+          <h1><?php ViewSumSpendingByCategory(3); ?></h1>
         </div>
 
         <div class="circle-card">
           <img src="assets/bus.png" />
           <h4>Ulaşım</h4>
-          <h1>200TL</h1>
+          <h1><?php ViewSumSpendingByCategory(4); ?></h1>
         </div>
 
         <div class="circle-card">
           <img src="assets/giftbox.png" />
           <h4>Hediye</h4>
-          <h1>200TL</h1>
+          <h1><?php ViewSumSpendingByCategory(5); ?></h1>
         </div>
 
         <div class="circle-card">
           <img src="assets/restaurant.png" />
           <h4>Restorant</h4>
-          <h1>200TL</h1>
+          <h1><?php ViewSumSpendingByCategory(6); ?></h1>
         </div>
-        
+
       </div>
     </div>
   </body>
