fcbfcdc81e7a75ad763c42ae2a40ff6b756e73e3
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2020-12-27 16:31:13 +0300

    li tag'ine border radius verildi

--- headLinks.php
+++ headLinks.php
@@ -4,14 +4,11 @@ echo '
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<link rel="stylesheet" href="style/style.css">
-<link rel="icon" type="image/png" href="assets/calculate.png" />
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-
+<link rel="stylesheet" href="style/style.css">
 <script src="https://kit.fontawesome.com/ac794817c2.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+<link rel="icon" type="image/png" href="assets/calculate.png" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 ';
